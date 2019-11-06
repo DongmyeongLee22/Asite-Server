06b28935817611086d33e4fc5aaf7c8fbf13b572
lee <dm951125@Naver.com>
2019-11-06 22:42:20 +0900

    무중단 배포 적용 테스트

--- src/main/java/me/asite/AsiteApplication.java
+++ src/main/java/me/asite/AsiteApplication.java
@@ -2,12 +2,18 @@ package me.asite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class AsiteApplication {
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/asite/real-application.yml";
 
     public static void main(String[] args) {
-        SpringApplication.run(AsiteApplication.class, args);
+        new SpringApplicationBuilder(AsiteApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
 }
